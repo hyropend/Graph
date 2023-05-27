b2f390eb744d5c8c265d925e3fdb674242c93a43
Buğrahan Yaşar <emrebugrahanyasar@gmail.com>
2023-05-27 16:34:16 +0300

    GraphImp

---
+++ Main.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class Graph {
+    private int vertices;
+    private List<List<Integer>> adjacencyList;
+
+    public Graph(int vertices) {
+        this.vertices = vertices;
+        adjacencyList = new ArrayList<>(vertices);
+        for (int i = 0; i < vertices; i++) {
+            adjacencyList.add(new ArrayList<>());
+        }
+    }
+
+    public void addEdge(int source, int destination) {
+        adjacencyList.get(source).add(destination);
+    }
+
+    public void BFS(int startVertex) {
+        boolean[] visited = new boolean[vertices];
+
+        Queue<Integer> queue = new LinkedList<>();
+        visited[startVertex] = true;
+        queue.offer(startVertex);
+
+        while (!queue.isEmpty()) {
+            int currentVertex = queue.poll();
+            System.out.print(currentVertex + " ");
+
+            List<Integer> neighbors = adjacencyList.get(currentVertex);
+            for (int neighbor : neighbors) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.offer(neighbor);
+                }
+            }
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Graph graph = new Graph(7);
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 5);
+        graph.addEdge(2, 6);
+
+        System.out.println("BFS Traversal (starting from vertex 0):");
+        graph.BFS(0);
+    }
+}
